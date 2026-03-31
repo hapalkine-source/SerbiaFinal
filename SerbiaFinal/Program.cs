f6e275cafb5465db54c8a27b46009145421bbd58
Хапалкин Игорь <garikhapa@mail.ru>
2026-03-31 20:26:17 +0300

    мал да удал

--- SerbiaFinal/Program.cs
+++ SerbiaFinal/Program.cs
@@ -1,7 +1,6 @@
 // Подключаем работу с базой данных
 using Microsoft.EntityFrameworkCore;
 using SerbiaFinal;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Добавляем поддержку контроллеров
@@ -13,6 +12,11 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", policy => {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -24,6 +28,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+app.UseCors("AllowAll");
+app.UseDefaultFiles(); // Чтобы сервер искал index.html сам
+app.UseStaticFiles();  // Чтобы сервер разрешил отдавать файлы из папки wwwroot
+
 app.MapControllers();
 
 app.Run();
